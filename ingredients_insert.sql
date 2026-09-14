413b16ecb697be9c8534e16d0e0132555014aafb
rongogola <ronhoward.gogola.cvt@eac.edu.ph>
2026-09-14 17:06:36 +0800

    Update ingredients_insert.sql

--- ingredients_insert.sql
+++ ingredients_insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO ingredients (ingredient_name, stock_quantity) VALUES 
-('Secret Krabby Patty Formula', 9999),
-('Sea Onion Slices', 150),
-('Sea Cheese', 200),
-('Pickles', 85),
-('Kelp Flour Bun', 300);
+INSERT INTO ingredients (ingredient_id, ingredient_name, stock_quantity) VALUES 
+(1, 'Secret Krabby Patty Formula', 9999),
+(2, 'Sea Onion Slices', 150),
+(3, 'Sea Cheese', 200),
+(4, 'Pickles', 85),
+(5, 'Kelp Flour Bun', 300);
