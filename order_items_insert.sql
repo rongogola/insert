4f3a2e70c8b172a41d2f6ee5ecf5a57333e7de80
rongogola <ronhoward.gogola.cvt@eac.edu.ph>
2026-09-14 17:07:23 +0800

    Update order_items_insert.sql

--- order_items_insert.sql
+++ order_items_insert.sql
@@ -1,8 +1,6 @@
-INSERT INTO order_items (order_id, item_id, quantity) VALUES 
-(1, 1, 2), -- SpongeBob ordered 2 Krabby Patties
-(1, 3, 1), -- SpongeBob ordered 1 Kelp Shake
-(2, 2, 5), -- Patrick ordered 5 Double Krabby Patties
-(2, 4, 2), -- Patrick ordered 2 Coral Bits
-(3, 5, 1), -- Squidward ordered 1 Krusty Krab Pizza
-(4, 3, 1), -- Mr. Krabs ordered 1 Kelp Shake
-(5, 1, 3); -- Sandy ordered 3 Krabby Patties
+INSERT INTO order_items (order_item_id, order_id, item_id, quantity) VALUES 
+(1, 1, 1, 2),
+(2, 1, 3, 1),
+(3, 2, 2, 5),
+(4, 2, 4, 2),
+(5, 3, 5, 1);
