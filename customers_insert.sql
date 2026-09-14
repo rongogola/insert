180d6981d750e927ebe0b875e12b01a8bf214763
rongogola <ronhoward.gogola.cvt@eac.edu.ph>
2026-09-14 17:06:13 +0800

    Update customers_insert.sql

--- customers_insert.sql
+++ customers_insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO customers (name, phone, address) VALUES 
-('SpongeBob SquarePants', '555-123-PINE', '124 Conch Street'),
-('Patrick Star', '555-987-ROCK', '120 Conch Street'),
-('Squidward Tentacles', '555-456-TENT', '122 Conch Street'),
-('Eugene H. Krabs', '555-MONY-ANC', '3541 Anchor Way'),
-('Sandy Cheeks', '555-TREE-DOM', 'Sub-Dome, Outskirts');
+INSERT INTO customers (customer_id, name, phone, address) VALUES 
+(1, 'SpongeBob SquarePants', '555-123-PINE', '124 Conch Street'),
+(2, 'Patrick Star', '555-987-ROCK', '120 Conch Street'),
+(3, 'Squidward Tentacles', '555-456-TENT', '122 Conch Street'),
+(4, 'Eugene H. Krabs', '555-MONY-ANC', '3541 Anchor Way'),
+(5, 'Sandy Cheeks', '555-TREE-DOM', 'Sub-Dome, Outskirts');
