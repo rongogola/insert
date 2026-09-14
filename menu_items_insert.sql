a2228d431016dd3d0e6f0ff7d86ea53ee8fced14
rongogola <ronhoward.gogola.cvt@eac.edu.ph>
2026-09-14 17:07:01 +0800

    Update menu_items_insert.sql

--- menu_items_insert.sql
+++ menu_items_insert.sql
@@ -1,6 +1,6 @@
-INSERT INTO menu_items (item_name, category, price) VALUES 
-('Krabby Patty', 'Burgers', 2.99),
-('Double Krabby Patty', 'Burgers', 3.99),
-('Kelp Shake', 'Beverages', 2.00),
-('Coral Bits', 'Sides', 1.95),
-('Krusty Krab Pizza', 'Specialties', 4.99);
+INSERT INTO menu_items (item_id, item_name, category, price) VALUES 
+(1, 'Krabby Patty', 'Burgers', 2.99),
+(2, 'Double Krabby Patty', 'Burgers', 3.99),
+(3, 'Kelp Shake', 'Beverages', 2.00),
+(4, 'Coral Bits', 'Sides', 1.95),
+(5, 'Krusty Krab Pizza', 'Specialties', 4.99);
